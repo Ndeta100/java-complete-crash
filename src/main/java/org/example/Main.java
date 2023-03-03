919230a75bc2a5e07c4ffaf435fdc86637822800
ndeta <ndetainnocent3@gmail.com>
2023-03-03 04:04:10 +0200

    Diff between primitive and reference types

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -13,6 +13,30 @@ public class Main {
         float theFloat= 0.0030903333F;
         int theInt=213;
         long theLong=39939393993090234L;
+        //Reference types
+        String brand=new String("ndeta.io");
+        System.out.println(brand.toUpperCase());
 
+        //Diff between primitive and reference types
+        int a=10;
+        int b=a;
+        a=100;
+        System.out.println("a =" +a + " b=" +b);
+        Person alex=new Person("alex");
+        Person mariam=alex;
+        System.out.println("before changing alex");
+        System.out.println(alex.name + " " + mariam.name);
+
+        System.out.println("after changing alex");
+        alex.name="Alexandre";
+        System.out.println(alex.name + " " + mariam.name);
+
+    }
+    //Using reference type to demonstrate
+    static class Person{
+        String name;
+        Person(String name){
+            this.name=name;
+        }
     }
 }
